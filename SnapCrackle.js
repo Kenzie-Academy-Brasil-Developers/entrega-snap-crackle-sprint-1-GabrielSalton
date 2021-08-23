3f22b0b096e77b3c93c31dec335153b2463a4f6b
Gabriel Salton <gabriel.salton.donoso@hotmail.com>
2021-08-23 08:58:12 -0300

    Snap Crackle feito

---
+++ SnapCrackle.js
@@ -0,0 +1,24 @@
+function snapCrackle (maxValue){
+    let meuRetorno ="";
+
+    for (let counter = 1; counter <= maxValue; counter++){
+        if (counter % 2 === 0 && counter % 5 !== 0){
+            meuRetorno += counter + ", ";
+            continue;
+        }
+        if (counter % 2 !== 0 && counter % 5 === 0){
+            meuRetorno += "SnapCrackle, ";
+            continue;
+        }
+        if (counter % 5 === 0){
+            meuRetorno += "Crackle, ";
+            continue;
+        }
+        if (counter % 2 !== 0){
+            meuRetorno += "Snap, ";
+            continue;
+        }
+    }
+    return meuRetorno;
+}
+console.log(snapCrackle());
